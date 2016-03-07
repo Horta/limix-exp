0edb778a5e6e1e7731e908d8877986f8e638e55f
Danilo Horta <danilo.horta@gmail.com>
2016-03-07 16:02:47 +0000

    stats

--- limix_exp/qtl/task.py
+++ limix_exp/qtl/task.py
@@ -2,12 +2,13 @@ from limix_exp.task import TaskResult
 from numpy import asarray
 
 class QTLTaskResult(TaskResult):
-    __slots__  = ['_pv']
+    __slots__  = ['_pv', '_stats']
 
     def __init__(self, workspace_id, experiment_id, task_id):
         super(QTLTaskResult, self).__init__(workspace_id, experiment_id,
                                               task_id)
         self._pv = dict()
+        self._stats = dict()
 
     def pv(self, method):
         return self._pv[method]
@@ -17,3 +18,12 @@ class QTLTaskResult(TaskResult):
         pv = asarray(pv, float)
         assert pv.ndim == 1
         self._pv[method] = pv
+
+    def stats(self, method):
+        return self._stats[method]
+
+    def set_stats(self, method, stats):
+        self._add_method(method)
+        stats = asarray(stats, float)
+        assert stats.ndim == 1
+        self._stats[method] = stats
